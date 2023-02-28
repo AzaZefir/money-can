5e208ffa28645940c428beff01fe846ef5a8687e
azat <arykov.azat.kg@gmail.com>
2023-02-28 14:13:22 +0600

    add: cooperation section in cooperation page

--- src/components/cooperationPage/Cooperation.jsx
+++ src/components/cooperationPage/Cooperation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CooperationCatalog } from './cooperationCatalog/CooperationCatalog';
 import { CooperationProduction } from './cooperationProduction/CooperationProduction';
+import { CooperationWork } from './cooperationWork/CooperationWork';
 
 export const Cooperation = () => {
   return (
@@ -13,7 +14,9 @@ export const Cooperation = () => {
         <CooperationProduction/>
       </section>
 
-      
+      <section className="how-we-work">
+        <CooperationWork/>
+      </section>
     </section>
   );
 };
---
+++ src/components/cooperationPage/cooperationWork/CooperationWork.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import arr3 from '../../../assets/images/cooperationImg/Arrow 3.svg';
+import workSvg from '../../../assets/images/cooperationImg/work.svg';
+
+export const CooperationWork = () => {
+  return (
+    <div className="container">
+      <div className='how-we-work__arrow'>
+        <img src={arr3} alt="" />
+      </div>
+      <div className='how-we-work__content'>
+        <div>
+          <img src={workSvg} alt="" />
+          <h4>КАК МЫ РАБОТАЕМ</h4>
+        </div>
+        <ol>
+          <li>Согласование пошива по образцу</li>
+          <li>Согласование цен изделий</li>
+          <li>Сроки отшива всей партии</li>
+          <li>Дополнительные параметры - лейблы, упаковка, этикетка и т.д</li>
+          <li>
+            Согласование фулфилмента
+            <ul>
+              <li>Маркировка</li>
+              <li>Упаковка в зип пакеты с нанесенным логотипом</li>
+              <li>Проверка на брак по всем регламентам</li>
+              <li>отправка </li>
+            </ul>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+};
