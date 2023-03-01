154446c858b7b4230e8008683daefab17e8b2e13
azat <arykov.azat.kg@gmail.com>
2023-03-01 18:30:37 +0600

    add: firebase data map function in catalog

--- src/components/catalog/Catalog.jsx
+++ src/components/catalog/Catalog.jsx
@@ -6,7 +6,7 @@ import { CatalogSort } from './catalogSort/CatalogSort';
 import { Button } from './../common/button/Button';
 import { CatalogPagination } from './catalogPagination/CatalogPagination';
 
-export const Catalog = () => {
+export const Catalog = ({catalogItems}) => {
   return (
     <div className="catalog">
       <div className="container">
@@ -15,7 +15,7 @@ export const Catalog = () => {
           <CatalogSort />
         </div>
         <CatalogFilter />
-        <CatalogCards />
+        <CatalogCards catalogItems={catalogItems}/>
       </div>
       <CatalogPagination/>
       <ContuctUs />
--- src/components/catalog/catalogCards/CatalogCards.jsx
+++ src/components/catalog/catalogCards/CatalogCards.jsx
@@ -3,42 +3,45 @@ import catalogItem1 from '../../../assets/images/catalogImg/catalog1.svg';
 import catalogItem2 from '../../../assets/images/catalogImg/catalog2.svg';
 import { Button } from './../../common/button/Button';
 
-export const CatalogCards = () => {
+export const CatalogCards = ({ catalogItems }) => {
   return (
     <div className="catalog__cards">
-      <div className="catalog__card">
-        <div>
-          <img src={catalogItem1} alt="" />
+      {catalogItems.map((item) => (
+        <div className="catalog__card">
+          <div>
+            <img src={item.image} alt="" />
+          </div>
+          <h4>{item.title}</h4>
+          <p>Цена {item.price} руб.</p>
+          <div className="catalog__card__colors">
+            <span className="green"></span>
+            <span className="violet"></span>
+            <span className="red"></span>
+            <span className="dark"></span>
+            <span className="khaki"></span>
+          </div>
+          <div className="catalog__card__sizes">
+            <span className="">
+              <strong>xs</strong>
+            </span>
+            <span className="">
+              <strong>s</strong>
+            </span>
+            <span className="">
+              <strong>l</strong>
+            </span>
+            <span className="">
+              <strong>xl</strong>
+            </span>
+            <span className="">
+              <strong>xxl</strong>
+            </span>
+          </div>
+          <Button>добавить в корзину</Button>
         </div>
-        <h4>Футболка USA</h4>
-        <p>Цена 450 руб.</p>
-        <div className="catalog__card__colors">
-          <span className="green"></span>
-          <span className="violet"></span>
-          <span className="red"></span>
-          <span className="dark"></span>
-          <span className="khaki"></span>
-        </div>
-        <div className="catalog__card__sizes">
-          <span className="">
-            <strong>xs</strong>
-          </span>
-          <span className="">
-            <strong>s</strong>
-          </span>
-          <span className="">
-            <strong>l</strong>
-          </span>
-          <span className="">
-            <strong>xl</strong>
-          </span>
-          <span className="">
-            <strong>xxl</strong>
-          </span>
-        </div>
-        <Button>добавить в корзину</Button>
-      </div>
-      <div className="catalog__card">
+      ))}
+
+      {/* <div className="catalog__card">
         <div>
           <img src={catalogItem2} alt="" />
         </div>
@@ -389,7 +392,7 @@ export const CatalogCards = () => {
           </span>
         </div>
         <Button>добавить в корзину</Button>
-      </div>
+      </div> */}
     </div>
   );
 };
