e13c3ad8977736c98ff9886c36c7974dd9e41f53
azat <arykov.azat.kg@gmail.com>
2023-03-04 12:51:02 +0600

    fix: catalog adaptive

--- src/components/catalog/catalogFilter/CatalogFilter.jsx
+++ src/components/catalog/catalogFilter/CatalogFilter.jsx
@@ -3,12 +3,17 @@ import { Button } from './../../common/button/Button';
 
 export const CatalogFilter = () => {
   return (
-    <div className="catalog__category__filter">
+    <div className="catalog__category__filter" id='style__scroll'>
       <Button>Все</Button>
       <Button>Пальто</Button>
       <Button>Свитшоты</Button>
       <Button>Кардиганы</Button>
       <Button>Толстовки</Button>
+      <Button>Под заказ</Button>
+      <Button>Под заказ</Button>
+      <Button>Под заказ</Button>
+      <Button>Под заказ</Button>
+      <Button>Под заказ</Button>
       <Button>Под заказ</Button>
     </div>
   );
